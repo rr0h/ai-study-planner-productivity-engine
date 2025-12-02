f04574cffc848775f3fa8367a66f50c3c40f5583
Rohit <119391810+rr0h@users.noreply.github.com>
2025-12-03 01:51:05 +0530

    Add WSGI configuration

---
+++ study_planner/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for study_planner project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_planner.settings')
+
+application = get_wsgi_application()
