e0640bd4f72b5c5938a0610e9b741e359f5d01ce
Rohit <119391810+rr0h@users.noreply.github.com>
2025-12-03 01:51:05 +0530

    Add ASGI configuration

---
+++ study_planner/asgi.py
@@ -0,0 +1,11 @@
+"""
+ASGI config for study_planner project.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_planner.settings')
+
+application = get_asgi_application()
